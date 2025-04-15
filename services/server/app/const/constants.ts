91538b91e902945ed2257946dccf27b10d46d838
petarzarkov <pzarko1@gmail.com>
2025-04-15 10:49:01 +0300

    fix allowedOrigins

--- services/server/app/const/constants.ts
+++ services/server/app/const/constants.ts
@@ -4,5 +4,5 @@ export const allowedOrigins = [
   'http://127.0.0.1:3033',
   'http://localhost:3033',
   'http://localhost:5173',
-  'https://derp.ai.petarzarkov.com/',
+  'https://derp.ai.petarzarkov.com',
 ];
